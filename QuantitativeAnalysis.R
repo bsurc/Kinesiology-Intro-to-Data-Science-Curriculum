552dd50cbc84d9eacd97ea785731f91bd441f5b9
Matt Clark <matthewclark989@boisestate.edu>
2019-11-29 09:10:26 -0700

    made some glms

--- QuantitativeAnalysis.R
+++ QuantitativeAnalysis.R
@@ -45,7 +45,7 @@ summary(fit)
 fit <- lm(Variable_1 ~ Treatment, data=dat)
 summary(fit)
 plot(fit)
-
+hist(residuals(fit)) #this should be relatively normal
 #lm with both continuous
 ggplot(dat, aes(x=Variable_4, y=Variable_1))+
   geom_point()+
@@ -55,18 +55,84 @@ ggplot(dat, aes(x=Variable_4, y=Variable_1))+
 
 fit <- lm(Variable_1 ~ Variable_4, data=dat)
 summary(fit)
+hist(residuals(fit))
+
+
+#We can look at multivariable regressions too
+
+#Let's look at all of our continuous variables as a predictr for var 1
+
+fit<-lm(Variable_1~Variable_4+Variable_3+Variable_2+Treatment, data=dat)
+summary(fit)
+
+#Note that variable 2 has the highest p value
+# remove that first
+fit<-lm(Variable_1~Variable_4+Variable_3+Treatment, data=dat)
+summary(fit)
+
+#Now note that the p values for the other variables has changed
+#How have they changed?
+#They got lower! That's why it's important to remove the variables one at a time
+fit<-lm(Variable_1~Variable_4+Treatment, data=dat)
+summary(fit)
+
+#We see now that none of our continuous variables are important
+
+#Treatment is though.
+
+#why are there only two treatments displayed?
+
+#TreatmentA is the intercept!
+
+fit<-lm(Variable_1~Treatment, data=dat)
+summary(fit)
+
+
+#summary gives us some parameter estimates. What do these mean?
+
+#Let's check out the means to get a better idea
+mean(dat[dat$Treatment=="A",]$Variable_1)
+mean(dat[dat$Treatment=="B",]$Variable_1)
+mean(dat[dat$Treatment=="C",]$Variable_1)
 
 #now control for sex
+#You can make a 3d plot or a 
+#density plot to plot 2 variable regressions
+
+#We're not going to show these
+fit<-lm(Variable_1~Sex+Treatment, data=dat)
+summary(fit)
+
+#a better way to do this is to give each sex 
+#its own intercept, slope, or both
+
+#let's plot both
+
 ggplot(dat, aes(x=Variable_4, y=Variable_1, color=Sex))+
   geom_point()+
   geom_smooth(method="lm")+
   theme_classic()
 
+#we need a new package to fit this model 
 fit <- lmer(Variable_1 ~ Variable_4+(Variable_4|Sex), data=dat)
 summary(fit)
 
+
+#If we wanted each sex to just have its own intercept
+fit <- lmer(Variable_1 ~ Variable_4+(1|Sex), data=dat)
+summary(fit)
+plot(fit)
+
 #show this
 #http://mfviz.com/hierarchical-models/
+
+#Make some predictions
+#this is a really powerful way to predict things, but also
+#to gutcheck yourself!
+new <- data.frame(Variable_4 = 25, Treatment="B", Sex="Male")
+predict(fit, new)
+
+
 
 
 
